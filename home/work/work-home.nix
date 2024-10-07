e345c6ac0c14e374fda934ac37ba6881f1ca02d6
jimalexberger <jim-alexander.berger.seterdahl@nrk.no>
2024-10-07 14:36:10 +0200

    add kitty config

--- home/work/work-home.nix
+++ home/work/work-home.nix
@@ -203,6 +203,13 @@
     };
   };
 
+  programs.kitty = {
+    enable = true;
+    font.name = "JetBrainsMono";
+    shellIntegration.enableZshIntegration = true;
+    themeFile = "Catppuccin-Frappe";
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
