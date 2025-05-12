a58d3ad0a90ab0855a66ad185c757ef98dbf3b56
jimalexberger <jim-alexander.berger.seterdahl@nrk.no>
2025-05-12 13:06:38 +0200

    auto accept cert on first connect

--- home/work/freerdp.nix
+++ home/work/freerdp.nix
@@ -9,5 +9,5 @@
     sopsFile = ../../secrets/rdpservers/rdpservers;
   };
 
-  programs.zsh.shellAliases.rdpwork = "cat ${config.sops.secrets.rdpservers.path} | ${pkgs.fzf}/bin/fzf --with-nth '{1}' --delimiter , | cut -d , -f 2 | xargs -I{} xfreerdp /u:tmp651227 /d:felles /v:{}";
+  programs.zsh.shellAliases.rdpwork = "cat ${config.sops.secrets.rdpservers.path} | ${pkgs.fzf}/bin/fzf --with-nth '{1}' --delimiter , | cut -d , -f 2 | xargs -I{} xfreerdp /u:tmp651227 /d:felles /cert:tofu /v:{}";
 }
